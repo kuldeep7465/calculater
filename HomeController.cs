d24b5f4a347f0387441283685eae00f7912e1319
kuldeep7465 <91813803+kuldeep7465@users.noreply.github.com>
2022-05-06 17:56:31 +0530

    Add files via upload

---
+++ HomeController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvcautomaticCrud.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+        public ActionResult Calculat()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Calculat(string firstNumber, string secondNumber, string Cal)
+        {
+            int a = int.Parse(firstNumber);
+            int b = int.Parse(secondNumber);
+            int c = 0;
+            switch (Cal)
+            {
+                case "Add":
+                    c = a + b;
+                    break;
+                case "Sub":
+                    c = a - b;
+                    break;
+                case "Mul":
+                    c = a * b;
+                    break;
+                case "Div":
+                    c = a / b;
+                    break;
+            }
+            ViewBag.Result = c;
+            return View();
+        }
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
